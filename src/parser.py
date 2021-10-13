dbbec69b948ff33443d97aa4bad86da38111d58a
bowwoden <william.f.bowden@gmail.com>
2021-10-13 13:12:00 -0700

    Second passing test case. Tests a function that finds the values, not arguments, in the string. Created a test case for returning values for undefined arguments.

--- src/parser.py
+++ src/parser.py
@@ -6,11 +6,26 @@ class Parser:
     def __init__(self):
         pass
 
-    def parse_flags(self, arguments: str):
+    def parse_argument(self, arguments: str):
+        values = "blah"
+        return values
 
+    def parse_flags(self, arguments: str):
         # characters minus plus a-Z define an argument
-        args_regex = '-[a-zA-Z]*'
+        args_regex = r'-[a-zA-Z]*'
 
         # returns all substrings matching the regex
         return re.findall(args_regex, arguments)
 
+    def parse_values(self, arguments: str):
+
+        args_regex = r'-[a-zA-Z]*'
+        spaces_regex = r' '
+
+        values_not_flags = list()
+
+        for element in re.split(spaces_regex, arguments):
+            if (re.fullmatch(args_regex, element)) is None:
+                values_not_flags.append(element)
+
+        return values_not_flags
--- tests/test_args.py
+++ tests/test_args.py
@@ -6,7 +6,7 @@ from src.parser import Parser
 class MyTestCase(unittest.TestCase):
 
     def test_parse_arguments(self):
-        arguments: str = "-l -d 8080 -d /usr/var"
+        arguments: str = "-l -p 8080 -d /usr/var"
 
         parser: Parser = Parser()
 
@@ -14,10 +14,36 @@ class MyTestCase(unittest.TestCase):
 
         print(flags)
 
-        expected_args = ['-l', '-d', '-d']
+        expected_args = ['-l', '-p', '-d']
 
         self.assertEqual(flags, expected_args)
 
+    def test_parse_values(self):
+        arguments: str = "-l -p 8080 -d /usr/var"
+
+        parser: Parser = Parser()
+
+        values = parser.parse_values(arguments)
+
+        print(values)
+
+        expected_values = ['8080', '/usr/var']
+
+        self.assertEqual(values, expected_values)
+
+    def test_parse_return_values_for_undefined_args(self):
+        arguments: str = "-p 8080 -d /usr/var"
+
+        # in this test, it should return false for logging
+        # this return is because the -l flag is not used
+
+        parser: Parser = Parser()
+
+        parsed_args_values = parser.parse_argument()
+
+        expected_values = ["LOGGING:FALSE", "PORT:8080", "DIRECTORY:/usr/var"]
+
+        self.assertEqual(expected_values, parsed_args_values)
 
 
 if __name__ == '__main__':
